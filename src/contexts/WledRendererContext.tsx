d0dbfa3dd91a159bde6ea0ba0e1a8b4cc22cc328
Will Martin <williammartinthird@gmail.com>
2025-11-06 00:21:35 -0700

    Fix WLED render race bug

--- src/contexts/WledRendererContext.tsx
+++ src/contexts/WledRendererContext.tsx
@@ -1,11 +1,16 @@
 import {
   createContext,
+  createRef,
   PropsWithChildren,
+  useCallback,
   useContext,
   useEffect,
   useState,
 } from 'react';
 
+import { toJsonString } from '@bufbuild/protobuf';
+import { PatchSchema } from '@dmx-controller/proto/output_pb';
+import { Project } from '@dmx-controller/proto/project_pb';
 import { WledOutput } from '@dmx-controller/proto/wled_pb';
 import { renderWled } from '../engine/renderRouter';
 import { sendWled } from '../system_interfaces/wled';
@@ -20,53 +25,68 @@ export function WledRendererProvider({ children }: PropsWithChildren) {
   const { project, update } = useContext(ProjectContext);
   const [warnings, setWarnings] = useState<{ [outputId: string]: string }>({});
 
-  const startRenderLoop = (outputId: bigint) => {
-    let cont = true;
-    let frame = 0;
-    (async () => {
-      const wledOutput = getActivePatch(project).outputs[outputId.toString()]
-        .output.value as WledOutput;
-
-      const latencySamples: number[] = [];
-
-      while (cont) {
-        const startMs = new Date().getTime();
-        const renderTarget = await renderWled(outputId, frame++);
-
-        try {
-          await sendWled(wledOutput.ipAddress, renderTarget);
-          latencySamples.push(new Date().getTime() - startMs);
-        } catch (e) {
-          console.error(e);
-          const output = getOutput(project, outputId);
-          setWarnings(
-            Object.assign({}, warnings, {
-              [outputId.toString()]: `Could not connect to WLED device ${output.name}!`,
-            }),
-          );
-        }
-
-        if (latencySamples.length >= 40) {
-          const total = latencySamples.reduce((a, b) => a + b);
-          const latency = Math.floor(total / latencySamples.length / 2);
-          getOutput(project, outputId).latencyMs = latency;
-          latencySamples.length = 0;
-          update();
-        }
-      }
-    })();
-
-    return () => (cont = false);
-  };
+  const projectRef = createRef<Project>();
 
   useEffect(() => {
+    projectRef.current = project;
+  }, [project]);
+
+  const startRenderLoop = useCallback(
+    (outputId: bigint) => {
+      let cont = true;
+      let frame = 0;
+      (async () => {
+        const latencySamples: number[] = [];
+
+        while (cont) {
+          const project = projectRef.current;
+          if (!project) {
+            return;
+          }
+          const wledOutput = getActivePatch(project).outputs[
+            outputId.toString()
+          ].output.value as WledOutput;
+
+          const startMs = new Date().getTime();
+          const renderTarget = await renderWled(outputId, frame++);
+
+          try {
+            await sendWled(wledOutput.ipAddress, renderTarget);
+            latencySamples.push(new Date().getTime() - startMs);
+          } catch (e) {
+            console.error(e);
+            const output = getOutput(project, outputId);
+            setWarnings(
+              Object.assign({}, warnings, {
+                [outputId.toString()]: `Could not connect to WLED device ${output.name}!`,
+              }),
+            );
+          }
+
+          if (latencySamples.length >= 40) {
+            const total = latencySamples.reduce((a, b) => a + b);
+            const latency = Math.floor(total / latencySamples.length / 2);
+            getOutput(project, outputId).latencyMs = latency;
+            latencySamples.length = 0;
+            update();
+          }
+        }
+      })();
+
+      return () => (cont = false);
+    },
+    [projectRef],
+  );
+
+  useEffect(() => {
+    console.log('New loops');
     const renderLoops: Array<() => void> = [];
     Object.entries(getActivePatch(project).outputs)
       .filter(([_, output]) => output.output.case === 'wledOutput')
       .forEach(([id, _]) => renderLoops.push(startRenderLoop(BigInt(id))));
 
     return () => renderLoops.forEach((f) => f());
-  }, [project]);
+  }, [toJsonString(PatchSchema, getActivePatch(project))]);
 
   return (
     <WledRendererContext.Provider value={{ warnings }}>
